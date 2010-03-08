95a9058aed2101fc75ed399dde5a5afbba725406
levente.meszaros <levente.meszaros@gmail.com>
2010-03-08 13:29:50 +0000

    Package

--- documentation/package.lisp
+++ documentation/package.lisp
@@ -4,9 +4,9 @@
 ;;;
 ;;; See LICENCE for details.
 
-(in-package :common-lisp-user)
+(in-package :hu.dwim.def)
 
-(defpackage :hu.dwim.debug.documentation
+(def package :hu.dwim.debug.documentation
   (:use :hu.dwim.asdf
         :hu.dwim.common
         :hu.dwim.debug
